e6eb192e3fd848228b06011cbc4fcba62b0d5af7
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-02 14:36:55 -0800

    push, pall

--- main.c
+++ main.c
@@ -4,55 +4,62 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 1024
-
+/**
+ * main - Entry point for the Monty interpreter program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always returns 0
+ */
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    char line[MAX_LINE_LENGTH];
-    char *opcode;
-    char *arg;
-    int line_number = 0;
-    stack_t *stack = NULL;
+	FILE *file;
+	char line[MAX_LINE_LENGTH];
+	char *opcode;
+	char *arg;
+	int line_number = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	stack_t *stack = NULL;
 
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        line_number++;
-        line[strcspn(line, "\n")] = '\0';
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-        opcode = strtok(line, " ");
-        if (opcode == NULL)
-            continue;
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+		line_number++;
+		line[strcspn(line, "\n")] = '\0';
 
-        if (strcmp(opcode, "push") == 0)
-        {
-            arg = strtok(NULL, " ");
-            if (arg == NULL)
-            {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-            push(&stack, atoi(arg));
-        }
-        else if (strcmp(opcode, "pall") == 0)
-        {
-            pall(&stack);
-        }
-    }
+		opcode = strtok(line, " ");
+		if (opcode == NULL)
+			continue;
 
-    fclose(file);
+		if (strcmp(opcode, "push") == 0)
+		{
+			arg = strtok(NULL, " ");
+			if (arg == NULL)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				exit(EXIT_FAILURE);
+			}
+			push(&stack, atoi(arg));
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(&stack);
+		}
+	}
 
-    return (0);
+	fclose(file);
+
+	return (0);
 }
