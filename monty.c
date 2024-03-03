a334eaba9250180dc8df9b25b31f780371848b18
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-03 13:50:11 -0800

    pint

--- monty.c
+++ monty.c
@@ -63,20 +63,22 @@ void pint(stack_t **stack, int line_number)
  */
 void pop(stack_t **stack, int line_number)
 {
-	stack_t *temp;
+    stack_t *temp;
 
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	temp = *stack;
-	*stack = (*stack)->next;
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
+    temp = *stack;
+    *stack = (*stack)->next;
+    if (*stack != NULL)
+    {
+        (*stack)->prev = NULL;
+    }
 
-	free(temp);
+    free(temp);
 }
 /**
  * swap - Swaps the top two elements of the stack
