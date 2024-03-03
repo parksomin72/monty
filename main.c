baeac7c79cc7ecc684a95e70535bf77d88370e45
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-02 16:27:03 -0800

    push, pall

--- main.c
+++ main.c
@@ -15,82 +15,83 @@
  */
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    char line[MAX_LINE_LENGTH];
-    char *opcode;
-    char *arg;
-    int line_number = 0;
-    stack_t *stack = NULL;
+	FILE *file;
+	char line[MAX_LINE_LENGTH];
+	char *opcode;
+	char *arg;
+	int line_number = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	stack_t *stack = NULL;
 
-    file = fopen(argv[1], "r");
-    if (file == NULL)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        line_number++;
-        line[strcspn(line, "\n")] = '\0';
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-        opcode = strtok(line, " ");
-        if (opcode == NULL)
-            continue;
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+		line_number++;
+		line[strcspn(line, "\n")] = '\0';
 
-        if (strcmp(opcode, "push") == 0)
-        {
-            arg = strtok(NULL, " ");
-            if (arg == NULL || !is_numeric(arg))
-            {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                fclose(file);
-                free_stack(stack);
-                exit(EXIT_FAILURE);
-            }
-            push(&stack, atoi(arg));
-        }
-        else if (strcmp(opcode, "pall") == 0)
-        {
-            pall(&stack);
-        }
-        else
-        {
-            fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-            fclose(file);
-            free_stack(stack);
-            exit(EXIT_FAILURE);
-        }
-    }
+		opcode = strtok(line, " ");
+		if (opcode == NULL)
+			continue;
 
-    fclose(file);
-    free_stack(stack);
-    return (0);
+		if (strcmp(opcode, "push") == 0)
+		{
+			arg = strtok(NULL, " ");
+			if (arg == NULL || !is_numeric(arg))
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line_number);
+				fclose(file);
+				free_stack(stack);
+				exit(EXIT_FAILURE);
+			}
+			push(&stack, atoi(arg));
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(&stack);
+		}
+		else
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+			fclose(file);
+			free_stack(stack);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	fclose(file);
+	free_stack(stack);
+	return (0);
 }
 
 /**
- * is_numeric - Checks if a string is a valid numeric value
+ * is_numeric - Checks if a string is a valid integer
  * @str: The string to check
  *
- * Return: 1 if the string is numeric, 0 otherwise
+ * Return: 1 if the string is a valid integer, 0 otherwise
  */
 int is_numeric(const char *str)
 {
-    if (str == NULL || *str == '\0')
-        return 0;
+	if (str == NULL || *str == '\0')
+		return 0;
 
-    while (*str != '\0')
-    {
-        if (!isdigit(*str))
-            return 0;
-        str++;
-    }
+	while (*str != '\0')
+	{
+		if (!isdigit(*str) && *str != '-' && *str != '+')
+			return 0;
+		str++;
+	}
 
-    return (1);
+	return (1);
 }
--- stack_functions.c
+++ stack_functions.c
@@ -1,14 +1,20 @@
 #include "monty.h"
 #include <stdlib.h>
 
+/**
+ * free_stack - Frees a stack.
+ * @stack: Pointer to the top of the stack
+ *
+ * This function frees all the nodes in the stack.
+ */
 void free_stack(stack_t *stack)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    while (stack != NULL)
-    {
-        temp = stack;
-        stack = stack->next;
-        free(temp);
-    }
+	while (stack != NULL)
+	{
+		temp = stack;
+		stack = stack->next;
+		free(temp);
+	}
 }
