d989366afd0507a6d073f822fee81692ffa828ba
parksomin72 <fatimazahraealawi@gmail.com>
2024-03-03 21:56:33 -0800

    nop

--- stack_functions1.c
+++ stack_functions1.c
@@ -140,14 +140,3 @@ void add(stack_t **stack, int line_number)
     (*stack)->prev = NULL;
     free(temp);
 }
-
-/**
- * nop - Doesn't do anything.
- * @stack: Double pointer to the stack
- * @line_number: Line number in the Monty file
- */
-void nop(stack_t **stack, int line_number)
-{
-	(void) stack;
-	(void) line_number;
-}
---
+++ stack_functions2.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+#include <stdio.h>
+
+/**
+ * nop - Doesn't do anything.
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the Monty file
+ */
+void nop(stack_t **stack, int line_number)
+{
+    (void) stack;
+    (void) line_number;
+}
